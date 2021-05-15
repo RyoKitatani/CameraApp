12f4b4b5f10ed07e322d006fc2c1cda9c29f631b
RyoKitatani <ryo0929m@gmail.com>
2021-05-15 15:23:15 +0900

    演習10-3完了

---
+++ CameraApp/ImagePicker.swift
@@ -0,0 +1,32 @@
+//
+//  ImagePicker.swift
+//  CameraApp
+//
+//  Created by RyoKitatani on 2021/05/15.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController,
+                                context: Context) {
+    }
+}
+
+struct ImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePicker()
+    }
+}
