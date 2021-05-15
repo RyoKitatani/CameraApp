3add7c35e240247d5332fed4d03cd3d93c2a68c5
RyoKitatani <ryo0929m@gmail.com>
2021-05-15 10:08:16 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by RyoKitatani on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
